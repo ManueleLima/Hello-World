b535189889c3230f26fc205ff5de10a84cda816a
Manuele Lima <manuele20lima@gmail.com>
2022-09-25 16:53:03 -0300

    Projeto Hello

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
     public static void main(String args[]){
         System.out.print("Ufa!! Meu primeiro projeto em java!!\n");
         System.out.print("Java eh massa!!\n");
+        
     }
 }
