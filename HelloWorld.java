d4c87303e12dbb39f0e2da9720c628e12944f743
Manuele Lima <manuele20lima@gmail.com>
2022-09-26 19:35:27 -0300

    proj HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 public class HelloWorld{
-    public static void main(String args[]){
-        System.out.print("Ufa!! Meu primeiro projeto em java!!\n");
-        System.out.print("Java eh massa!!\n");
+
+    public static void main(String[] args){
+        System.out.println("Olá Mundo  \n");
+        System.out.println("Meu projeto Java \n");
+        System.out.println("Sou uma programadora \n");
+        System.out.println("Obaa!!! Agora posso criar qualquer coisa!! \n");
+
         
     }
 }
---
+++ Readme.md
@@ -0,0 +1,1 @@
+Exercicio Hello World
